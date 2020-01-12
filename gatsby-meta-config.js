f8e5e2dedde10da6dc915f2bff29b556984462e2
miiiiiin <min.songkyung@gmail.com>
2020-01-12 18:29:41 +0900

    info setting

---
+++ gatsby-meta-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `Raccoon's devlog`,
+  description: ``,
+  author: `miiiiiin`,
+  introduction: `이것저것 삽질합이것저것 삽질합니다`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/miiiiiin`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
